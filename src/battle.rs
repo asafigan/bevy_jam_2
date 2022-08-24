dd7cf0434f73619dea1574cb8d2b646eeaf23497
Andrew Safigan <asafigan@gmail.com>
2022-08-24 17:48:02 +0900

    play idle animation

--- src/battle.rs
+++ src/battle.rs
@@ -1,4 +1,4 @@
-use bevy::{asset::HandleId, prelude::*, render::view::RenderLayers};
+use bevy::{animation, asset::HandleId, prelude::*, render::view::RenderLayers};
 use strum::{EnumCount, IntoEnumIterator};
 use strum_macros::{Display, EnumCount, EnumIter, EnumVariantNames};
 
@@ -8,7 +8,8 @@ pub struct BattlePlugin;
 
 impl Plugin for BattlePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(load_enemy_models);
+        app.add_startup_system(load_enemy_models)
+            .add_system(play_idle_animation);
     }
 }
 
@@ -20,6 +21,34 @@ fn load_enemy_models(asset_server: Res<AssetServer>, mut commands: Commands) {
     let models = asset_server.load_folder("models/enemies").unwrap();
 
     commands.insert_resource(EnemyModels { models });
+}
+
+fn play_idle_animation(
+    enemies: Query<(Entity, &Enemy)>,
+    children: Query<&Children>,
+    mut animations: Query<&mut AnimationPlayer, Added<AnimationPlayer>>,
+) {
+    'enemies: for (entity, enemy) in &enemies {
+        for child in children.get(entity).into_iter().flatten() {
+            if let Ok(mut animation_player) = animations.get_mut(*child) {
+                animation_player
+                    .play(enemy.kind.idle_animation_handle())
+                    .repeat();
+
+                continue 'enemies;
+            }
+
+            for child in children.get(*child).into_iter().flatten() {
+                if let Ok(mut animation_player) = animations.get_mut(*child) {
+                    animation_player
+                        .play(enemy.kind.idle_animation_handle())
+                        .repeat();
+
+                    continue 'enemies;
+                }
+            }
+        }
+    }
 }
 
 pub struct BattlePrefab {
@@ -50,11 +79,19 @@ pub struct EnemyPrefab {
 
 impl Prefab for EnemyPrefab {
     fn construct(&self, entity: Entity, commands: &mut Commands) {
-        commands.entity(entity).insert_bundle(SceneBundle {
-            scene: self.kind.scene_handle(),
-            ..default()
-        });
+        commands
+            .entity(entity)
+            .insert_bundle(SceneBundle {
+                scene: self.kind.scene_handle(),
+                ..default()
+            })
+            .insert(Enemy { kind: self.kind });
     }
+}
+
+#[derive(Component)]
+pub struct Enemy {
+    kind: EnemyKind,
 }
 
 #[derive(Clone, Copy, EnumVariantNames, EnumIter, EnumCount, Display)]
@@ -91,6 +128,12 @@ impl EnemyKind {
 
     pub fn scene_handle(&self) -> Handle<Scene> {
         let path = format!("models/enemies/{self}.glb#Scene0");
+
+        Handle::weak(HandleId::AssetPathId(path.as_str().into()))
+    }
+
+    pub fn idle_animation_handle(&self) -> Handle<AnimationClip> {
+        let path = format!("models/enemies/{self}.glb#Animation5");
 
         Handle::weak(HandleId::AssetPathId(path.as_str().into()))
     }
